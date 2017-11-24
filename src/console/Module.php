053ba2046d336e72d34a9da9bca5265c580ce24a
Vitaliy <theyamshikov@yandex.ru>
2017-11-24 20:01:26 +0600

    init files

---
+++ src/console/Module.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace yii2lab\db\console;
+
+use yii\base\Module as YiiModule;
+
+class Module extends YiiModule
+{
+	
+}
---
+++ src/console/controllers/InitController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace yii2lab\db\console\controllers;
+
+use yii2lab\console\yii\console\Controller;
+
+class InitController extends Controller
+{
+	
+	
+	
+}
