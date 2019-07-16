7ae83f331e98e450087bd8bd683e1190e22bfd2e
amubarak <amubarak@wooppay.com>
2019-07-16 16:37:45 +0600

    create yii2 db generate all migration by schema

--- src/console/controllers/MigrationController.php
+++ src/console/controllers/MigrationController.php
@@ -2,6 +2,7 @@
 
 namespace yii2lab\db\console\controllers;
 
+use yii\db\pgsql\Schema;
 use yii2lab\extension\console\helpers\input\Enter;
 use yii2lab\extension\console\helpers\Output;
 use yii2lab\extension\console\base\Controller;
@@ -22,5 +23,15 @@ class MigrationController extends Controller
 		$className = MigrationHelper::generateByTableName($tableName);
 		Output::block($className, 'Migration created!');
 	}
-	
+	/**
+	 * Generate migration with columns and foreign keys
+	 */
+	public function actionGenerateAll()
+	{
+		$schema = \Yii::$app->db->getSchema();
+		foreach ($schema->getTableSchemas() as $tableName => $tableSchema){
+			$className = MigrationHelper::generateByTableName($tableSchema->name);
+			Output::block($className, 'Migration created!');
+		}
+	}
 }
